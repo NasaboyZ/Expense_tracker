45c9ea2eda746d526c12f958056c37ffbbfc144c
NazaboyZ <josefleite.00@hotmail.com>
2026-02-28 14:57:31 +0100

    chore: implemnted numeric Keybords and modal

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -24,13 +25,26 @@ class _ExpensesState extends State<Expenses> {
       category: Category.lesure,
     ),
   ];
+
+  _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) {
+        return const NewExpense();
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Expense Tracker'),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.add_box_rounded)),
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: Icon(Icons.add_box_rounded),
+          ),
         ],
       ),
       body: Column(
