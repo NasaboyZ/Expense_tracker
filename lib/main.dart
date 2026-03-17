a8ba89c0ac9850986ee847884b5272a6c8f41d8e
NazaboyZ <josefleite.00@hotmail.com>
2026-03-17 19:55:33 +0100

    chore: chard is responsive

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,31 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+
 var kColorSheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData().copyWith(
-        useMaterial3: true,
-        colorScheme: kColorSheme,
-        appBarTheme: const AppBarTheme().copyWith(
-            backgroundColor: kColorSheme.onPrimaryContainer,
-            foregroundColor: kColorSheme.primary),
-        cardTheme: const CardTheme().copyWith(
-            color: kColorSheme.secondaryContainer,
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-                backgroundColor: kColorSheme.primaryContainer)),
-        textTheme: ThemeData().textTheme.copyWith(
-            titleLarge: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: kColorSheme.onSecondaryContainer,
-                fontSize: 16))),
-    home: const Expenses(),
-  ));
+  WidgetsFlutterBinding.ensureInitialized();
+
+    runApp(MaterialApp(
+      theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: kColorSheme,
+          appBarTheme: const AppBarTheme().copyWith(
+              backgroundColor: kColorSheme.onPrimaryContainer,
+              foregroundColor: kColorSheme.primary),
+          cardTheme: const CardTheme().copyWith(
+              color: kColorSheme.secondaryContainer,
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: kColorSheme.primaryContainer)),
+          textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: kColorSheme.onSecondaryContainer,
+                  fontSize: 16))),
+      home: const Expenses(),
+    ));
+
 }
